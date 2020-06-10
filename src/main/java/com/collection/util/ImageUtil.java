8658ddc4e085c1ed89d708f2897193b9d9bf518b
silence <lucky_markli@sina.com>
2020-06-10 22:51:03 +0800

    连接服务器zookeeper

--- src/main/java/com/collection/util/ImageUtil.java
+++ src/main/java/com/collection/util/ImageUtil.java
@@ -1,31 +1,23 @@
 package com.collection.util;
 
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 
 import org.apache.log4j.Logger;
 
-import com.collection.controller.PhoneGapServlet;
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
