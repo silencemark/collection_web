af22596ce8eb80b21d2885bfe66eec7880426b35
liaoqh <lyaojin@163.com>
2020-07-11 14:05:38 +0800

    xx

---
+++ src/main/webapp/app/video/js/dwn.js
@@ -0,0 +1,99 @@
+var commentList = [
+	{
+		tx:'http://img5.imgtn.bdimg.com/it/u=183326193,1784969774&fm=26&gp=0.jpg',
+		name:'爱玩呼啦圈的小喵喵',commTime:'2020/04/02',xin:3,version:'5.3.4.1248',
+		contentTxt:'感觉还不错，但这个任务卡也太TM难得抢了吧，靠，靠，靠',
+		zan:208, nzhan:5
+	},{
+		tx:'http://img5.imgtn.bdimg.com/it/u=3526059149,524929962&fm=26&gp=0.jpg',
+		name:'ai芙兰',commTime:'2020/03/28',xin:4,version:'5.3.4.1248',
+		contentTxt:'有要领任务赚奖励的朋友加我好友，做我的粉丝我保证我宠你。V : yys3563,快点来快点来等我升级到V10再来的话就不宠你了',
+		zan:20, nzhan:0
+	},{
+		tx:'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2034740944,4251903193&fm=26&gp=0.jpg',
+		name:'W DeGaio',commTime:'2020/03/25',xin:1,version:'5.3.2.101',
+		contentTxt:'为什么更新了老卡屏，有时候还闪退，差评能不能不给星，烦死人。',
+		zan:0, nzhan:0
+	},{
+		tx:'http://img1.imgtn.bdimg.com/it/u=2579660820,1492564183&fm=11&gp=0.jpg',
+		name:'小飞同学',commTime:'2020/03/23',xin:5,version:'5.2.0.111',
+		contentTxt:'非常棒，领取任务卡两下子做完任务就赚了，我这里有领取任务秘籍联系我教你领抢任务手慢无哦',
+		zan:1, nzhan:0
+	},{
+		tx:'http://img2.imgtn.bdimg.com/it/u=903791629,504409189&fm=11&gp=0.jpg',
+		name:'小飞同学',commTime:'2020/01/02',xin:3,version:'5.2.0.111',
+		contentTxt:'我就想知道主页里广告的那个女猪脚是谁，三分钟之内我要她的去全部信息，否则锤死在做的各位，egm',
+		zan:12, nzhan:0
+	}
+];
+$(function(){
+	$(".dwOptionEl").click(function(){
+		$(".dwOptionEl").removeClass("dwOptionActive");
+		$(this).addClass("dwOptionActive");
+		if($(this).attr("tp")=="1"){
+			$("#appInfo").show()
+			$("#appcomment").hide()
+		}else{
+			$("#appInfo").hide()
+			$("#appcomment").show()
+		}
+	});
+	
+	var commHtml = "";
+	for(var i=0;i<commentList.length;i++){
+		var c = commentList[i];
+		var xinhtml = "";
+		for(var j=1;j<=5;j++){
+			if(j<=c.xin){
+				xinhtml +='<img src="img/sXin.png" class="xin" />';
+			}else{
+				xinhtml +='<img src="img/eXin.png" class="xin" />';
+			}
+		}
+		var eHtml = 
+			'<div class="dwcommentCont">'+
+				'<div class="dwLeft"> <img src="'+c.tx+'"/> </div>'+
+				'<div class="dwright">'+
+					'<div style="color:#393838;font-size:14px;">'+c.name+'</div>'+
+					'<div style="color:#939393;font-size:12px;margin-top: 2px;">'+
+						''+c.commTime+''+
+					'</div>'+
+				'</div>'+
+				'<div class="clear"></div>'+
+				'<div class="dwCommx">'+xinhtml+
+					'<span style="color:#939393;font-size:12px;margin-left:20px;">版本号：'+c.version+'</span>'+
+				'</div>'+
+				'<div style="font-size:13px;color:#4f4f4f;padding:10px 10px 10px 0;">'+
+					''+c.contentTxt+''+
+				'</div>'+
+				'<div class="bwzhan">'+
+					'<img src="img/notzany.png" onclick="zan(this);" />&nbsp;<span>'+c.zan+'</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+					'<img src="img/notzann.png"  onclick="nzan(this);" />&nbsp;<span>'+c.nzhan+'</span>'+
+				'</div>'+
+				'<div class="clear"></div>'+
+			'</div>';
+		commHtml += eHtml;
+	}
+	commHtml += '<div style="font-size:13px;color:#777;padding:10px 10px 20px 0;text-align:center">下拉刷新</div>';
+	$("#appcomment").html(commHtml);
+})
+function zan(obj){
+	var z = parseInt($(obj).next().html());
+	if($(obj).attr("src").indexOf("not")>=0){
+		$(obj).attr("src","img/zanys.png")
+		$(obj).next().html(z+1);
+	}else{
+		$(obj).attr("src","img/notzany.png");
+		$(obj).next().html(z-1);
+	}
+}
+function nzan(obj){
+	var z = parseInt($(obj).next().html());
+	if($(obj).attr("src").indexOf("not")>=0){
+		$(obj).attr("src","img/zanns.png")
+		$(obj).next().html(z+1);
+	}else{
+		$(obj).attr("src","img/notzann.png");
+		$(obj).next().html(z-1);
+	}
+}
